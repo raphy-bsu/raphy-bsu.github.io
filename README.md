7518cc30e9ce5b3e0460b176456160cd1934ca58
Orel Sokolov <orelcokolov@gmail.com>
2014-11-22 21:20:05 +0300

    Add redis cache

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ raphy-bsu.github.io
 ===================
 
 Raphy bsu landing page
+
+
+
+## Dependencies
+
+* Jekyll
+* redis-server (localhost)
--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,14 +1,23 @@
+require 'redis'
+
 module Jekyll
   module GithubAvatar
     def avatar(username)
-      def full_name
+      def full_name username
         require 'json'
         require 'net/http'
-        JSON.parse(Net::HTTP.get URI('https://api.github.com/users/OrelSokolov'))["name"]
+        JSON.parse(Net::HTTP.get URI("https://api.github.com/users/#{username}"))["name"]
       end
-      "<a href='https://github.com/#{username}' class='hint--left' data-hint='#{full_name}'>
-        <img src='https://avatars.githubusercontent.com/#{username}' alt='#{username}'/>
-      </a>"
+
+      def html username, full_name
+        "<a href='https://github.com/#{username}' class='hint--left' data-hint='#{full_name}'>
+          <img src='https://avatars.githubusercontent.com/#{username}' alt='#{username}'/>
+        </a>"
+      end
+
+      redis = Redis.new
+      f_name = redis.exists username ? redis.get username : full_name username
+      html username, f_name
     end
   end
 end
