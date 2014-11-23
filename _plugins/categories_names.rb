b3a5a9c186f5ec8a5be385a201aba827714fa2dd
Orel Sokolov <orelcokolov@gmail.com>
2014-11-23 12:52:02 +0300

    Remove cache for yaml file

--- _plugins/categories_names.rb
+++ _plugins/categories_names.rb
@@ -5,8 +5,7 @@ module Jekyll
   module CategoriesNames
     def category_name(name)
       filename = File.expand_path(File.join(__FILE__, "../../", "categories.yml"))
-      stat = File.stat(filename).atime
-      config = lazy("categories-#{stat}"){ YAML.load_file filename }
+      config = YAML.load_file filename
       config[name] || name
     end
   end
